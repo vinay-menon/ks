ef416570f346ae4ef23a25a7bd0f38a62ffb5a1b
vinay <dannylibrarian@gmail.com>
2018-02-25 20:39:36 +0530

    add other pages: artform-profile, start dev on booking

--- js/main-nav.js
+++ js/main-nav.js
@@ -1,18 +1,21 @@
 var $navTrigger = $(".main-nav-trigger button");
 
 var $mainHeader = $(".main-header");
+var elem = document.querySelector(".main-header");
+var theCSSprop = window.getComputedStyle(elem,null).getPropertyValue("height");
 
 $navTrigger.on("click", function(e){
     e.preventDefault();
-    console.log("clicked");
     $mainHeader.toggleClass("nav-open");
     $(this).toggleClass("is-active");
     $("body").toggleClass("no-scroll");
     $(".main-nav").toggleClass("is-hidden");
     if ($(this).hasClass("is-active")) {
         $(this).html("close");
+        $("body").attr("style", "padding-top: " + theCSSprop + "");
         } else {
             $(this).html("menu");
+            $("body").attr("style", "");
         }
 });
 
@@ -22,5 +25,7 @@ $(document).on("keydown", function(e){
         $mainHeader.removeClass("nav-open");
         $navTrigger.removeClass("is-active").html("menu");
         $(".main-nav").addClass("is-hidden");
+        $("body").removeClass("no-scroll");
+        $("body").attr("style", "");
         }
-})
+});
---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(function () {
+
+    var sticky = $('.content-header-lnk-group');
+
+    var top = sticky.offset().top - parseFloat(sticky.css('margin-top').replace(/auto/, 0));
+
+    $(window).on('scroll', function(event) {
+        // what the y position of the scroll is
+        var y = $(this).scrollTop();
+
+        // whether that's below the form
+        if (y >= top) {
+            // if so, ad the fixed class
+            if ( sticky.is('.is-fixed') ) {
+                return;
+            }
+            sticky.addClass('is-fixed');
+        } else {
+            // otherwise remove it
+            sticky.removeClass('is-fixed');
+        }
+    });
+
+});
+
+// REFACTOR
+// TODO: VERIFY BEFORE NEXT STEP
+
+$(".progress-1").on("click", function(e) {
+    e.preventDefault();
+    $("#content").removeClass("on-step-2").removeClass("on-step-3").addClass("on-step-1");
+});
+$(".progress-2").on("click", function(e) {
+    e.preventDefault();
+    $("#content").removeClass("on-step-1").removeClass("on-step-3").addClass("on-step-2");
+});
+$(".progress-3").on("click", function(e) {
+    e.preventDefault();
+    $("#content").removeClass("on-step-1").removeClass("on-step-2").addClass("on-step-3");
+});
+
+$("body").on("click", ".artform-item", function(e) {
+    e.preventDefault();
+
+    var $label = $(this).attr('data-label');
+    var $minMembers = $(this).attr('data-members');
+
+	if($(this).hasClass("is-selectable")){
+		$(this).removeClass("is-selectable").addClass("is-selected");
+		$(".selected-artforms-group").prepend("<div class='selected-artform " + $label + "'>" + "<label><span class='selected-artform-name'>" + $label + "</span><span class='selected-artform-msg'>No. of Artists Required:</span></label>" + "<input type='number' min='" + $minMembers + "' value='" + $minMembers + "'></div>");
+	} else if ($(this).hasClass("is-selected")) {
+		$(".selected-artforms-group ."+$label).remove();
+		$(this).removeClass("is-selected").addClass("is-selectable");
+	   }
+    if ($(".selected-artform").length > 0) {
+        $("#form-part-2").addClass("is-active");
+        } else if ($(".selected-artforms-group").not(":has(.selected-artform)")) {
+            $("#form-part-2").removeClass("is-active");
+            }
+});
